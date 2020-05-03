0bc2748674524fb6cbc6aff4568b5f298484ac8e
Manoel Tindão <timdao02@hotmail.com>
2020-05-03 17:57:26 -0300

    Criação do projeto

---
+++ src/main/java/com/tindao/cobranca2/Cobranca2Application.java
@@ -0,0 +1,13 @@
+package com.tindao.cobranca2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cobranca2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cobranca2Application.class, args);
+	}
+
+}
