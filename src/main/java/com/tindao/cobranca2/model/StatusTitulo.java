851e63fc5564e9301cf5a2734bb23066a849ef75
Manoel Tindão <timdao02@hotmail.com>
2020-05-04 10:57:00 -0300

    Criação da Entidade

---
+++ src/main/java/com/tindao/cobranca2/model/StatusTitulo.java
@@ -0,0 +1,19 @@
+package com.tindao.cobranca2.model;
+
+public enum StatusTitulo 
+{
+	PENDENTE("Pendente"),
+	RECEBIDO("Recebido");
+	
+	private String descricao;
+	
+	private StatusTitulo(String descricao) 
+	{
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao()
+	{
+		return descricao;
+	}
+}
